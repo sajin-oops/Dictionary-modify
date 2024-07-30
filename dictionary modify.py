6bbf58d0662be369c3e3e8c9b4ff92fcb7e02b0b
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-30 20:29:15 +0530

    Add files via upload

---
+++ dictionary modify.py
@@ -0,0 +1,9 @@
+#Dictionary in python
+a = {"name" : "Sajin", "age" :21 , "Gender":"male"}
+print(a.values())
+#O/P - dict_values(['Sajin', 21, 'male'])
+a["age"] = 20
+a["skill"] = "python"
+print(a)
+#After modify - {'name': 'Sajin', 'age': 20, 'Gender': 'male'}
+#{'name': 'Sajin', 'age': 20, 'Gender': 'male', 'skill': 'python'}
